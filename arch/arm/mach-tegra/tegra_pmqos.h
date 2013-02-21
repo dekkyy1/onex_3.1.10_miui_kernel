975341849b5536e8e76c6cad8a8c9be9e66c7985
Timothy Nelson <nelson.ecp@gmail.com>
2013-02-20 19:59:22 -0800

    mach-tegra: tegra_pmqos: Bringing values in line with 100mhz steps an…

--- arch/arm/mach-tegra/tegra_pmqos.h
+++ arch/arm/mach-tegra/tegra_pmqos.h
@@ -27,9 +27,16 @@
 
 /* in kHz */
 #define BOOST_CPU_FREQ_MIN	1700000
-#define CAP_CPU_FREQ_MAX	475000
 #define T3_CPU_MIN_FREQ     51000
 #define T3_LP_MAX_FREQ     	475000
+#define BOOST_CPU_FREQ_MIN	1500000
+#define CAP_CPU_FREQ_MAX	400000
+
+/* in 10 MHz
+ * this effectively makes all tablets 1600Mhz capable, but is limited by
+ * the clock definitions in tegra_dvfs.c */
+#define T3_VARIANT_BOOSTX       30
+#define T3_VARIANT_BOOST0       20
 
 extern unsigned int tegra_pmqos_boost_freq;
 extern unsigned int tegra_pmqos_cap_freq;
