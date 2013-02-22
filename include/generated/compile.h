32411b4e898ac332b271b22651d104e87044769c
dekkyy1 <dekkyyd@gmail.com>
2013-02-22 19:28:27 +0100

    Revert "Add XZ compression to zimage"
    This reverts commit fd6390cd3952e57acebae71b01ca343a57a5b5fc.

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 11 */
+/* This file is auto generated, version 6 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#11 SMP PREEMPT Fri Feb 22 19:18:03 CET 2013"
+#define UTS_VERSION "#6 SMP PREEMPT Fri Feb 22 12:18:35 CET 2013"
 #define LINUX_COMPILE_BY "dejan"
 #define LINUX_COMPILE_HOST "dejan-HP-ProBook-4730s"
 #define LINUX_COMPILER "gcc version 4.7.3 20130102 (prerelease) (Linaro GCC 4.7-2013.01) "
