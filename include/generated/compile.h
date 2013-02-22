7fa1f8a91158fd7d8add08a5e921020aec8642bb
dekkyy1 <dekkyyd@gmail.com>
2013-02-22 09:34:46 +0100

    Revert "Adding CPU Freq Hard Cap and Max Scaling Freq"
    This reverts commit cefa6ae0fae1469a7e96fa58f0668cc3e83461be.

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 0 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#0 SMP PREEMPT Fri Feb 22 01:55:15 CET 2013"
-#define LINUX_COMPILE_BY "dejan"
-#define LINUX_COMPILE_HOST "dejan-HP-ProBook-4730s"
-#define LINUX_COMPILER "gcc version 4.7.3 20130102 (prerelease) (Linaro GCC 4.7-2013.01) "
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "3.1.10-g45b76bb-dirty"
