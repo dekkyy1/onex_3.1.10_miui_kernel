e9552685dc61dcae0b8a85a1f9cc35bee2ddfc74
dekkyy1 <dekkyyd@gmail.com>
2013-02-22 09:46:00 +0100

    changes

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Fri Feb 22 09:40:07 CET 2013"
+#define LINUX_COMPILE_BY "dejan"
+#define LINUX_COMPILE_HOST "dejan-HP-ProBook-4730s"
+#define LINUX_COMPILER "gcc version 4.7.3 20130102 (prerelease) (Linaro GCC 4.7-2013.01) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.1.10-g7fa1f8a"
